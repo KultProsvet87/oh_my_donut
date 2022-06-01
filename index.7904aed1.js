42d687d0b870513a9a39a9c0a54d273e69658fbb
KultProsvet87 <kultprosvet87@gmail.com>
2022-06-01 20:05:30 +0000

    Deploying to gh-pages from @ KultProsvet87/oh_my_donut@6820e60cc52b2c2a409268db86699898450929a4 🚀

---
+++ index.7904aed1.js
@@ -0,0 +1,2 @@
+document.querySelector(".mob-menu__open-btn").addEventListener("click",(function(){document.querySelector(".mob-menu").classList.toggle("active")})),document.querySelector(".mob-menu__close-btn").addEventListener("click",(function(){document.querySelector(".mob-menu").classList.toggle("active")}));var menu__link=document.querySelectorAll(".mob-menu__link");function classToogle(){document.querySelector(".mob-menu").classList.toggle("active")}menu__link.forEach((function(e){e.addEventListener("click",classToogle)})),alert(1);
+//# sourceMappingURL=index.7904aed1.js.map
